111c04ef2183d58d87e39855e22cbd324c7aa5dc
Brian Canela <Canelabrian123@gmail.com>
2019-06-04 15:52:44 -0700

    added the leagues branch and model for leagues

---
+++ Futbol Source/Model/Leagues.swift
@@ -0,0 +1,15 @@
+//
+//  Leagues.swift
+//  Futbol Source
+//
+//  Created by Brian Canela on 6/4/19.
+//  Copyright © 2019 Brian Canela. All rights reserved.
+//
+
+import Foundation
+
+struct Leagues {
+    let league_id: String
+    let leagueTitle: String
+    let leagueLogo: String
+}
