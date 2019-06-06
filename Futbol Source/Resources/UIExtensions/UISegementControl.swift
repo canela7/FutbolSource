80964d678f4e91ab37488287203a51ef5dd24939
Brian Canela <Canelabrian123@gmail.com>
2019-06-06 11:49:01 -0700

    working with segemnt control

---
+++ Futbol Source/Resources/UIExtensions/UISegementControl.swift
@@ -0,0 +1,20 @@
+//
+//  UISegementControl.swift
+//  Futbol Source
+//
+//  Created by Brian Canela on 6/6/19.
+//  Copyright © 2019 Brian Canela. All rights reserved.
+//
+import UIKit
+
+
+class UISegementControlApperance: UISegmentedControl {
+
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        
+        
+    }
+    
+    
+}
